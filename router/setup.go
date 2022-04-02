2a801acf361dc1f0053b32865e80f68d4aca73e8
Ajith Hussain <ajith.hussain@fastmail.com>
2022-04-03 08:27:13 +1000

    Added logging

--- router/setup.go
+++ router/setup.go
@@ -1,9 +1,12 @@
 package router
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/color"
 )
 
 func Setup(e *echo.Echo) {
@@ -12,7 +15,13 @@ func Setup(e *echo.Echo) {
 		return c.String(http.StatusOK, "OK")
 	})
 
-	e.GET("/", func(c echo.Context) error {
+	app := e.Group("")
+	app.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: fmt.Sprintf("[%v] [${time_rfc3339}] Got ${method} on ${uri} from ${remote_ip}. Responded with ${status} in ${latency_human}.\n", color.Green("INFO")), //nolint:lll,revive
+	}))
+	app.Use(middleware.Recover())
+
+	app.GET("/", func(c echo.Context) error {
 		//nolint:wrapcheck
 		return c.String(http.StatusOK, "hello")
 	})
